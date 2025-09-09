b9e09506a28c17ed693217fef005a6899e298eee
NusretSalli <nusret.emirhan.salli@gmail.com>
2025-09-09 21:02:59 +0200

    name generator based on race and gender added

--- app.py
+++ app.py
@@ -1,15 +1,56 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+from pynames import GENDER, LANGUAGE
+from pynames.generators.elven import WarhammerNamesGenerator, DnDNamesGenerator
+from pynames.generators.goblin import GoblinGenerator
+from pynames.generators.korean import KoreanNamesGenerator
+from pynames.generators.mongolian import MongolianNamesGenerator
+from pynames.generators.orc import OrcNamesGenerator
+from pynames.generators.russian import PaganNamesGenerator
+from pynames.generators.scandinavian import ScandinavianNamesGenerator
 
 app = Flask(__name__)
+
+# Mapping races to pynames generators
+RACE_TO_GENERATOR = {
+    'dwarf': KoreanNamesGenerator(),
+    'elf': DnDNamesGenerator(),
+    'half-orc': OrcNamesGenerator(),
+    'gnome': GoblinGenerator(),
+    # Using a generic fantasy generator for races without a specific one
+    'human': ScandinavianNamesGenerator(),
+    'halfling': KoreanNamesGenerator(),
+    'dragonborn': KoreanNamesGenerator(),
+    'half-elf': WarhammerNamesGenerator(),  # Using Warhammer names for half-elves
+    'tiefling': PaganNamesGenerator(),
+}
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+@app.route('/generate_name')
+def generate_name():
+    race = request.args.get('race', 'human')
+    gender_str = request.args.get('gender', 'male')
+    
+    gender = GENDER.MALE if gender_str == 'male' else GENDER.FEMALE
+    
+    generator = RACE_TO_GENERATOR.get(race, ScandinavianNamesGenerator())
+    
+    # Not all generators support gender, so we handle that
+    try:
+        name = generator.get_name_simple(gender)
+    except TypeError:
+        name = generator.get_name_simple()
+
+    return jsonify(name=name)
+
 @app.route('/create_character', methods=['POST'])
 def create_character():
     character = {
         'name': request.form.get('name'),
+        'gender': request.form.get('gender'),
         'race': request.form.get('race'),
         'class': request.form.get('class'),
         'strength': request.form.get('strength'),
