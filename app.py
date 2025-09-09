e0082ef8ad09d668e7e5dd5f1a7e8def1e0b21f8
NusretSalli <nusret.emirhan.salli@gmail.com>
2025-09-09 22:12:38 +0200

    Added character database as well as character list to track created characters

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 from pynames import GENDER, LANGUAGE
 from pynames.generators.elven import WarhammerNamesGenerator, DnDNamesGenerator
 from pynames.generators.goblin import GoblinGenerator
@@ -7,8 +8,34 @@ from pynames.generators.mongolian import MongolianNamesGenerator
 from pynames.generators.orc import OrcNamesGenerator
 from pynames.generators.russian import PaganNamesGenerator
 from pynames.generators.scandinavian import ScandinavianNamesGenerator
+import os
 
 app = Flask(__name__)
+# Configure SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dnd_characters.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Character Model
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    gender = db.Column(db.String(20), nullable=False)
+    race = db.Column(db.String(50), nullable=False)
+    character_class = db.Column(db.String(50), nullable=False)
+    strength = db.Column(db.Integer, nullable=False)
+    dexterity = db.Column(db.Integer, nullable=False)
+    constitution = db.Column(db.Integer, nullable=False)
+    intelligence = db.Column(db.Integer, nullable=False)
+    wisdom = db.Column(db.Integer, nullable=False)
+    charisma = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'<Character {self.name}>'
+
+# Create the database tables
+with app.app_context():
+    db.create_all()
 
 # Mapping races to pynames generators
 RACE_TO_GENERATOR = {
@@ -48,22 +75,37 @@ def generate_name():
 
 @app.route('/create_character', methods=['POST'])
 def create_character():
-    character = {
-        'name': request.form.get('name'),
-        'gender': request.form.get('gender'),
-        'race': request.form.get('race'),
-        'class': request.form.get('class'),
-        'strength': request.form.get('strength'),
-        'dexterity': request.form.get('dexterity'),
-        'constitution': request.form.get('constitution'),
-        'intelligence': request.form.get('intelligence'),
-        'wisdom': request.form.get('wisdom'),
-        'charisma': request.form.get('charisma'),
-    }
-    # For now, we'll just print the character to the console
-    # In the future, you would save this to a database
-    print(character)
-    return "Character created! Check the console."
+    # Create a new character instance
+    new_character = Character(
+        name=request.form.get('name'),
+        gender=request.form.get('gender'),
+        race=request.form.get('race'),
+        character_class=request.form.get('class'),  # Note: using character_class because class is a reserved word
+        strength=int(request.form.get('strength')),
+        dexterity=int(request.form.get('dexterity')),
+        constitution=int(request.form.get('constitution')),
+        intelligence=int(request.form.get('intelligence')),
+        wisdom=int(request.form.get('wisdom')),
+        charisma=int(request.form.get('charisma'))
+    )
+    
+    # Add and commit to database
+    db.session.add(new_character)
+    db.session.commit()
+    
+    return redirect(url_for('view_characters'))
+
+@app.route('/characters')
+def view_characters():
+    characters = Character.query.all()
+    return render_template('characters.html', characters=characters)
+
+@app.route('/delete_character/<int:character_id>', methods=['POST'])
+def delete_character(character_id):
+    character = Character.query.get_or_404(character_id)
+    db.session.delete(character)
+    db.session.commit()
+    return redirect(url_for('view_characters'))
 
 if __name__ == '__main__':
     app.run(debug=True)
